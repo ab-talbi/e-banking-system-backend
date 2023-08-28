07f7b063db938e4939a8f0561fc2dac2f03881f2
ayoub <ayoubtalbi787@gmail.com>
2023-08-28 22:46:28 +0100

    fix: abonné service

--- src/main/java/com/adria/ayoub/gestiondesabonnesebankingbackend/services/AbonneService.java
+++ src/main/java/com/adria/ayoub/gestiondesabonnesebankingbackend/services/AbonneService.java
@@ -7,11 +7,42 @@ import com.adria.ayoub.gestiondesabonnesebankingbackend.entities.BackOffice;
 import java.util.Optional;
 
 public interface AbonneService {
+    /**
+     * Pour ajouter un abonné
+     * @param abonne
+     * @return un objet de type abonné
+     */
     Abonne ajouterAbonne(Abonne abonne);
+
+    /**
+     * Pour trouver un abonné avec l'id passé au parametre
+     * @param id de l'abonné
+     * @return Optional<Abonne>
+     */
     Optional<Abonne> trouverUnAbonneById(Long id);
+
+    /**
+     * Pour supprimer un abonné
+     * @param id de l'abonné à supprimer
+     */
     void supprimerAbonneById(Long id);
+
+    /**
+     * Pour supprimer tous les abonnés
+     */
     void supprimerTousLesAbonnes();
 
+    /**
+     * Pour trouver une agence
+     * @param agence_id de l'agence à trouver
+     * @return Optional<Agence>
+     */
     Optional<Agence> trouverUneAgenceById(Long agence_id);
+
+    /**
+     * Pour teouver un backoffice
+     * @param backoffice_id de backoffice
+     * @return Optional<BackOffice>
+     */
     Optional<BackOffice> trouverUnBackOfficeById(Long backoffice_id);
 }

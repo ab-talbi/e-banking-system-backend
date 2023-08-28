1f5d3272a69f8aec20150f937377feea2d6909c2
ayoub <ayoubtalbi787@gmail.com>
2023-08-28 22:50:56 +0100

    fix: offre service

--- src/main/java/com/adria/ayoub/gestiondesabonnesebankingbackend/services/OffreService.java
+++ src/main/java/com/adria/ayoub/gestiondesabonnesebankingbackend/services/OffreService.java
@@ -5,8 +5,29 @@ import com.adria.ayoub.gestiondesabonnesebankingbackend.entities.Offre;
 import java.util.Optional;
 
 public interface OffreService {
+
+    /**
+     * Pour ajouter un offre
+     * @param offre à ajouter
+     * @return objet de type offre
+     */
     Offre ajouterOffre(Offre offre);
+
+    /**
+     * Pour trouver un offre
+     * @param id de l'offre à trouver
+     * @return Optional<Offre>
+     */
     Optional<Offre> trouverUnOffreById(Long id);
+
+    /**
+     * Pour supprimer un offre
+     * @param id de l'offre à supprimer
+     */
     void supprimerOffreById(Long id);
+
+    /**
+     * Pour supprimer tous les offres
+     */
     void supprimerTousLesOffres();
 }
